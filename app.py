350f07846b8b061bbba8e9939fe50266415f7002
shrishti <shrishtigautam01@gmail.com>
2026-04-27 21:31:19 +0530

    Switch from spaCy to NLTK for text cleaning
    Replaced spaCy with NLTK for text processing and updated the clean_text function to remove stopwords.

--- app.py
+++ app.py
@@ -1,55 +1,43 @@
 import streamlit as st
 import pickle
 import re
-import spacy
-st.set_page_config(page_title="Sentiment Analysis")
+import nltk
+from nltk.corpus import stopwords
 
-# load spacy
-nlp = spacy.load("en_core_web_sm", disable=["parser","ner"])
+nltk.download('stopwords')
+
+st.set_page_config(page_title="Sentiment Analysis")
 
 # load files
 model = pickle.load(open("model.pkl", "rb"))
 vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
 le = pickle.load(open("label_encoder.pkl", "rb"))
 
-
 def clean_text(text):
-
     text = re.sub(r"http\S+", "", text)
     text = text.lower()
     text = re.sub(r"[^a-z\s]", "", text)
     text = re.sub(r"\s+", " ", text)
 
-    doc = nlp(text)
+    stop_words = set(stopwords.words('english'))
+    words = text.split()
+    words = [word for word in words if word not in stop_words]
 
-    tokens = [
-        token.lemma_
-        for token in doc
-        if not token.is_stop
-    ]
-
-    return " ".join(tokens)
-
+    return " ".join(words)
 
 st.title("✈ Airline Tweet Sentiment Analysis")
 
 tweet = st.text_area("Enter Tweet")
 
 if st.button("Predict"):
-
     cleaned = clean_text(tweet)
-
     vec = vectorizer.transform([cleaned])
-
     pred = model.predict(vec)
-
     label = le.inverse_transform(pred)[0]
 
     if label == "positive":
         st.success("Positive 😊")
-
     elif label == "negative":
         st.error("Negative 😡")
-
     else:
         st.info("Neutral 😐")
