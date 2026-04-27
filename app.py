3b01b555124ef666f0ba1b62f195d701ac51b4ec
shrishti <shrishtigautam01@gmail.com>
2026-04-27 21:28:06 +0530

    Remove NLTK and integrate spaCy for preprocessing
    Replaced NLTK with spaCy for text preprocessing.

--- app.py
+++ app.py
@@ -1,19 +1,7 @@
 import streamlit as st
 import pickle
 import re
-import nltk
-from nltk.corpus import stopwords
-import string
-
-nltk.download('stopwords')
-
-def preprocess(text):
-    stop_words = set(stopwords.words('english'))
-    text = text.lower()
-    text = "".join([char for char in text if char not in string.punctuation])
-    words = text.split()
-    words = [word for word in words if word not in stop_words]
-    return " ".join(words)
+import spacy
 st.set_page_config(page_title="Sentiment Analysis")
 
 # load spacy
